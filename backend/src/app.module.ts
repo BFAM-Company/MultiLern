67667fdf62001e51c40011b572649568dca07f2a
Jakub Szrama <kubuszrama@gmail.com>
2024-02-18 19:37:46 +0100

    fixing styles

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { TranslationsModule } from './translations/translations.module';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [UsersModule, PrismaModule, TranslationsModule],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
