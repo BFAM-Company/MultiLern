d868ccb636ba6e5452fcc1723b428cd1999c2d1d
Jakub Szrama <kubuszrama@gmail.com>
2024-02-18 19:38:42 +0100

    fixing formatting with prettier

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,36 +1,43 @@
-import { Body, Controller, Get, Param, Post, Delete, HttpCode } from "@nestjs/common";
-import { UsersService } from "./users.service";
-import { CreateUserDto } from "./dtos/create-user.dto";
-
+import {
+    Body,
+    Controller,
+    Get,
+    Param,
+    Post,
+    Delete,
+    HttpCode,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 //localhost:3000-(czy ile tam bedziemy mieli w dockerze itp)/users
 @Controller('users')
-export class UsersController{
+export class UsersController {
     private usersService;
 
-    constructor(usersServive: UsersService){
+    constructor(usersServive: UsersService) {
         this.usersService = usersServive;
     }
 
     @Get()
-    getAllUsers(){ //nie wiem czy nie useless ale bedzie najwyzej sie usunie 
+    getAllUsers() {
+        //nie wiem czy nie useless ale bedzie najwyzej sie usunie
         return this.usersService.getAll();
     }
 
-
     @Get('/:id')
-    getUser(@Param('id') id: string){ 
+    getUser(@Param('id') id: string) {
         return this.usersService.getByID(parseInt(id));
     }
 
     @Post()
-    addUser(@Body() body: CreateUserDto){
+    addUser(@Body() body: CreateUserDto) {
         return this.usersService.add(body.name, body.email);
     }
 
     @Delete('/:id')
     @HttpCode(204)
-    removeProduct(@Param('id') id: string){
+    removeProduct(@Param('id') id: string) {
         return this.usersService.removeById(parseInt(id));
     }
 }
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,34 +1,33 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
 
-//chwilowo bo mi sie nie chce z baza laczyc 
+//chwilowo bo mi sie nie chce z baza laczyc
 let tempUsers = [
-    {id: 1, name: 'goroncyMati',email: "goroncymati@example.com"},
-    {id: 2, name: 'HotKubus', email: "hotkubus@example.com"},
+    { id: 1, name: 'goroncyMati', email: 'goroncymati@example.com' },
+    { id: 2, name: 'HotKubus', email: 'hotkubus@example.com' },
 ];
 
-//tu bedzie kod z baza docelowo 
+//tu bedzie kod z baza docelowo
 
-@Injectable()  //element wstrzykiwalny 
-
-export class UsersService{
-    getAll(){
-        //tu bedzie jakis madry kodzik 
+@Injectable() //element wstrzykiwalny
+export class UsersService {
+    getAll() {
+        //tu bedzie jakis madry kodzik
         return tempUsers;
     }
 
-    getByID(id: number){
-        return tempUsers.find(x => x.id === id);
+    getByID(id: number) {
+        return tempUsers.find((x) => x.id === id);
     }
 
-    add(name: string, email: string){
-        let id = tempUsers[tempUsers.length-1].id+1;
-        
-        const newUser = {id, name, email};
+    add(name: string, email: string) {
+        const id = tempUsers[tempUsers.length - 1].id + 1;
+
+        const newUser = { id, name, email };
 
         tempUsers.push(newUser);
     }
 
-    removeById(id: number){
-        tempUsers = tempUsers.filter(x => x.id !== id);
+    removeById(id: number) {
+        tempUsers = tempUsers.filter((x) => x.id !== id);
     }
 }
