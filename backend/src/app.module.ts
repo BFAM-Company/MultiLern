7ddd2168df7c60dd3d3f707f8e62f0241b2566e3
Jakub Szrama <kubuszrama@gmail.com>
2024-02-24 17:10:24 +0100

    hot fix

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
-import { TranslationsModule } from './translations/translations.module';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 
 @Module({
-    imports: [UsersModule, PrismaModule, TranslationsModule, PostsModule],
+    imports: [UsersModule, PrismaModule, PostsModule],
     controllers: [AppController],
     providers: [AppService],
 })
