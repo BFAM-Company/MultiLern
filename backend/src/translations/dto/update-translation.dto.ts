28f322b3516c4567ef7e0f92aeb2f8bf772c75a7
Jakub Szrama <kubuszrama@gmail.com>
2024-02-24 17:08:44 +0100

    deleting translations CRUD

--- backend/src/translations/dto/update-translation.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateTranslationDto } from './create-translation.dto';
-
-export class UpdateTranslationDto extends PartialType(CreateTranslationDto) {}
