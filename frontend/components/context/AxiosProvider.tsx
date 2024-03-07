b1aa48aeaf78d5b75e0cb0902832422b52aa6f47
Jakub Szrama <kubuszrama@gmail.com>
2024-03-07 21:33:07 +0100

    added Auth, Axios and User Contextes

---
+++ frontend/components/context/AxiosProvider.tsx
@@ -0,0 +1,103 @@
+import React, { ReactNode, createContext, useContext } from "react";
+import axios, { AxiosInstance } from "axios";
+import { AuthContext } from "./AuthContext";
+import createAuthRefreshInterceptor from "axios-auth-refresh";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+// import * as Keychain from "react-native-keychain";
+
+interface AuthState {
+  accessToken: string | null;
+  refreshToken: string | null;
+}
+
+interface AxiosContextProps {
+  authAxios: AxiosInstance;
+  publicAxios: AxiosInstance
+}
+
+const AxiosContext = createContext<AxiosContextProps>({
+  authAxios: axios.create({
+    baseURL: "http://localhost:3001/auth",
+  }),
+  publicAxios: axios.create({
+    baseURL: "http://localhost:3001/",
+  })
+});
+
+const { Provider } = AxiosContext;
+
+const AxiosProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const authContext = useContext(AuthContext);
+
+  const authAxios = axios.create({
+    baseURL: "http://localhost:3001/auth",
+  });
+
+  const publicAxios = axios.create({
+    baseURL: "http://localhost:3001/",
+  });
+
+  authAxios.interceptors.request.use(
+    (config) => {
+      if (!config.headers.Authorization) {
+        config.headers.Authorization = `Bearer ${authContext?.getAccessToken()}`;
+      }
+
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
+  const refreshAuthLogic = (failedRequest: any) => {
+    const data = {
+      refreshToken: authContext?.authState.refreshToken,
+    };
+
+    const options = {
+      method: "POST",
+      data,
+      url: "http://localhost:3001/auth/refresh",
+    };
+
+    return axios(options)
+      .then(async (tokenRefreshResponse: any) => {
+        failedRequest.response.config.headers.Authorization =
+          "Bearer " + tokenRefreshResponse.data.accessToken;
+
+        authContext?.setAuthState({
+          ...authContext.authState,
+          accessToken: tokenRefreshResponse.data,
+        });
+
+        await AsyncStorage.setItem('auth',JSON.stringify({
+            accessToken: tokenRefreshResponse.data,
+            refreshToken: authContext?.authState.refreshToken,
+        }) )
+        return Promise.resolve();
+      })
+      .catch((e: any) => {
+        authContext?.setAuthState({
+          accessToken: null,
+          refreshToken: null,
+          authenticated: false
+        });
+      });
+  };
+
+  createAuthRefreshInterceptor(authAxios, refreshAuthLogic, {});
+
+  return (
+    <Provider
+      value={{
+        authAxios,
+        publicAxios,
+      }}
+    >
+      {children}
+    </Provider>
+  );
+};
+
+export { AxiosContext, AxiosProvider };
